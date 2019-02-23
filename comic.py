6e1a2e99798bdc7d67db5a5c613ff28e9608201b
Gerard <gerwis1998@gmail.com>
2019-02-23 18:05:46 +0100

    Little fixes and installer added

--- comic.py
+++ comic.py
@@ -2,14 +2,15 @@ import os
 
 import bs4
 import requests
-import cv2 as cv2
+import cv2
+import numpy as np
 
 
 def getComic():
     os.makedirs('comics', exist_ok=True)
     res = requests.get('http://explosm.net/rcg')
     res.raise_for_status()
-    soup = bs4.BeautifulSoup(res.text)
+    soup = bs4.BeautifulSoup(res.text, "html.parser")
     comic_url = 'http://' + soup.select('#rcg-comic img')[0].attrs['src'][2:]
     comic_res = requests.get(comic_url)
     comic_name = comic_url.split('/')[-1]
@@ -31,9 +32,10 @@ def getPanels(comic_name):
     img1[height1 - 18:height1, 0:34] = [255, 255, 255]
     cv2.imwrite('comics/' + comic_name.split('.')[0] + '_0.' + comic_name.split('.')[1], img0)
     cv2.imwrite('comics/' + comic_name.split('.')[0] + '_1.' + comic_name.split('.')[1], img1)
-    os.unlink('comics/'+comic_name)
+    os.unlink('comics/' + comic_name)
     return comic_name
 
+
 if __name__ == "__main__":
-    for i in range(0,100):
+    for i in range(0, 5):
         getPanels(getComic())
